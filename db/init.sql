c11599592f4e44b83acc79766f133e847484edb9
eaglelizard <whitedtd@gmail.com>
2024-10-27 22:08:02 -0600

    add sort_order to jcd_prod_credit_contrib

--- db/init.sql
+++ db/init.sql
@@ -109,9 +109,11 @@ create table jcd_prod_credit (
 create table jcd_prod_credit_contrib (
   jcd_prod_credit_contrib_id SERIAL PRIMARY KEY,
 
+  sort_order INT NOT NULL,
   jcd_prod_credit_id INT references jcd_prod_credit(jcd_prod_credit_id) NOT NULL,
   person_id INT references person(person_id),
   org_id INT references org(org_id),
+  UNIQUE(sort_order, jcd_prod_credit_id),
   
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP
