82f79ccb80308279ee1f294256ff31821e633f20
eaglelizard <whitedtd@gmail.com>
2024-10-27 21:02:26 -0600

    update jcd_project_image_sort unique constraint

--- db/init.sql
+++ db/init.sql
@@ -206,10 +206,11 @@ create table jcd_producer_sort (
 
 create table jcd_project_image_sort (
   jcd_project_image_sort_id SERIAL PRIMARY KEY,
-  sort_order INT NOT NULL UNIQUE deferrable initially immediate,
+  sort_order INT NOT NULL,
 
   jcd_project_image_id INT references jcd_project_image(jcd_project_image_id) NOT NULL,
   jcd_project_id INT references jcd_project(jcd_project_id) NOT NULL,
+  UNIQUE (jcd_project_image_id, jcd_project_id, sort_order),
 
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP
