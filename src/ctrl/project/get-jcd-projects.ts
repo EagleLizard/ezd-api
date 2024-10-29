b024aca944984de147db4b0636011270dfc976bd
eaglelizard <whitedtd@gmail.com>
2024-10-29 16:51:22 -0600

    update get-jcd-projects endpoint

--- src/ctrl/project/get-jcd-projects.ts
+++ src/ctrl/project/get-jcd-projects.ts
@@ -1,11 +1,11 @@
 
 import type { FastifyReply, FastifyRequest } from 'fastify';
-import { JcdProjectService } from '../../service/jcd-project-service';
+import { JcdProjectDb } from '../../service/db/jcd-project-db';
 
 export async function getJcdProjectsCtrl(
   req: FastifyRequest,
   res: FastifyReply,
 ) {
-  let jcdProjects = await JcdProjectService.getProjects();
+  let jcdProjects = await JcdProjectDb.getProjects();
   return res.status(200).send(jcdProjects);
 }
