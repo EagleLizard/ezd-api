79969d327109ba700b2c7d1c3285458a8e2ab20b
eaglelizard <whitedtd@gmail.com>
2024-10-29 20:29:41 -0600

    add jcd-press-db service client

---
+++ src/service/db/jcd-press-db.ts
@@ -0,0 +1,24 @@
+import { PgClient } from '../../lib/db/postgres-client';
+import { JcdPressDto } from '../../lib/models/dto/jcd-press-dto';
+
+export const JcdPressDb = {
+  getProjectPress,
+} as const;
+
+async function getProjectPress(jcd_project_id: number) {
+  let queryStr = `
+    SELECT
+      jprs.jcd_press_id, jprs.description, jprs.link_text, jprs.link_url,
+      pb."name" as publication_name
+    FROM jcd_press jprs
+      INNER JOIN publication pb
+        ON jprs.publication_id = pb.publication_id
+    WHERE jprs.jcd_project_id = $1
+    ORDER BY jprs.sort_order ASC
+  `;
+  let res = await PgClient.query(queryStr, [
+    jcd_project_id,
+  ]);
+  let jcdPressDtos = res.rows.map(JcdPressDto.parse);
+  return jcdPressDtos;
+}
